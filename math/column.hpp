d443bc8cb5a26e49abc548f9c1016d33f355c4be
iamdramaticleo <iamdramaticleo@gmail.com>
2025-11-22 00:40:55 +0200

    Fix spacing inconsistencies in `column` operator definitions for improved readability and code style.

--- math/column.hpp
+++ math/column.hpp
@@ -4,11 +4,11 @@ namespace math
 {
     struct column
     {
-        auto operator+(const column& other) const -> column;
-        auto operator*(float         value) const -> column;
+        auto operator+ (const column& other) const -> column;
+        auto operator* (float         value) const -> column;
 
-        auto operator+=(const column& other)      -> column&;
-        auto operator*=(float         value)      -> column&;
+        auto operator+=(const column& other)       -> column&;
+        auto operator*=(float         value)       -> column&;
 
         float x { };
         float y { };
