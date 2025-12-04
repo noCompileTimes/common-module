7ead71e439e9c438db805eccc2325bfb51fafff4
iamdramaticleo <iamdramaticleo@gmail.com>
2025-12-04 22:44:54 +0200

    Refactor `mat4::perspective` to simplify FOV calculation and fix spacing inconsistencies.

--- math/mat4.cpp
+++ math/mat4.cpp
@@ -36,9 +36,8 @@ namespace math
 
     auto mat4::perspective(const float fov, const float aspect, const float near, const float far) -> void
     {
-         const auto a =  radians(fov);
-         const auto t =  tan(a * 0.5f);
-         const auto r =  far   - near;
+         const auto t =  tan(radians(fov) * 0.5f);
+         const auto r =  far - near;
 
         _columns[0].x =  1.0f / (t * aspect);
         _columns[1].y =  1.0f /  t;
