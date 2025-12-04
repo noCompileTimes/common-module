5164a188d53c81dce7cf336399ac54e1e6678f32
iamdramaticleo <iamdramaticleo@gmail.com>
2025-12-04 21:24:43 +0200

    Fix spacing inconsistency in `mat4::perspective` for improved code style.

--- math/mat4.hpp
+++ math/mat4.hpp
@@ -11,7 +11,7 @@ namespace math
         auto ortho(float left, float right, float bottom, float top, float near, float far) -> void;
         auto ortho(float left, float right, float bottom, float top)                        -> void;
 
-        auto perspective(float fov, float aspect, float near, float far)                    -> void;
+        auto perspective(float fov, float aspect, float near, float far) -> void;
 
         auto   translate(const vec3& vec) -> void;
         auto       scale(const vec3& vec) -> void;
