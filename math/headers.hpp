c8523ace9caea3f4ae1b25d25870c3b2860f57e9
iamdramaticleo <iamdramaticleo@gmail.com>
2025-12-05 15:29:17 +0200

    Reorganize includes in `headers.hpp` for improved readability and logical grouping

--- math/headers.hpp
+++ math/headers.hpp
@@ -1,14 +1,16 @@
 #pragma once
 
 #include <array>
+#include <numbers>
 
 #include <cmath>
 
+#include "constants.hpp"
 #include "functions.hpp"
-
-#include "rgb.hpp"
 
 #include "vec2.hpp"
 #include "vec3.hpp"
 
 #include "mat4.hpp"
+
+#include "rgb.hpp"
