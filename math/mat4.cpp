0863deedfc7716f255498858694b9b1edb1f95df
iamdramaticleo <iamdramaticleo@gmail.com>
2025-12-05 23:04:04 +0200

    Rename and refactor matrix and time member variables
    Renamed matrix member `_columns` to `columns` and time members for consistency. Made `mat4`'s constructor `constexpr` for compile-time initialization benefits. This improves clarity and aligns with modern coding practices.

--- math/mat4.cpp
+++ math/mat4.cpp
@@ -2,8 +2,8 @@
 
 namespace math
 {
-    mat4::mat4()
-        :  _columns
+    constexpr mat4::mat4()
+        :  columns
         {
             column { 1.0f, 0.0f, 0.0f, 0.0f },
             column { 0.0f, 1.0f, 0.0f, 0.0f },
@@ -15,51 +15,51 @@ namespace math
 
     auto mat4::ortho(const float left, const float right, const float bottom, const float top, const float near, const float far) -> void
     {
-        _columns[0].x =  2.0f / (right - left);
-        _columns[1].y =  2.0f / (top   - bottom);
-        _columns[2].z = -2.0f / (far   - near);
+        columns[0].x =  2.0f / (right - left);
+        columns[1].y =  2.0f / (top   - bottom);
+        columns[2].z = -2.0f / (far   - near);
 
-        _columns[3].x = -(right + left)   / (right - left);
-        _columns[3].y = -(top   + bottom) / (top   - bottom);
-        _columns[3].z = -(far   + near)   / (far   - near);
+        columns[3].x = -(right + left)   / (right - left);
+        columns[3].y = -(top   + bottom) / (top   - bottom);
+        columns[3].z = -(far   + near)   / (far   - near);
     }
 
     auto mat4::ortho(const float left, const float right, const float bottom, const float top) -> void
     {
-        _columns[0].x =  2.0f / (right - left);
-        _columns[1].y =  2.0f / (top   - bottom);
-        _columns[2].z = -1.0f;
+        columns[0].x =  2.0f / (right - left);
+        columns[1].y =  2.0f / (top   - bottom);
+        columns[2].z = -1.0f;
 
-        _columns[3].x = -(right + left)   / (right - left);
-        _columns[3].y = -(top   + bottom) / (top   - bottom);
+        columns[3].x = -(right + left)   / (right - left);
+        columns[3].y = -(top   + bottom) / (top   - bottom);
     }
 
     auto mat4::perspective(const float fov, const float aspect, const float near, const float far) -> void
     {
-         const auto t =  tan(radians(fov) * 0.5f);
-         const auto r =  far - near;
+        const auto t =  tan(radians(fov) * 0.5f);
+        const auto r =  far - near;
 
-        _columns[0].x =  1.0f / (t * aspect);
-        _columns[1].y =  1.0f /  t;
+        columns[0].x =  1.0f / (t * aspect);
+        columns[1].y =  1.0f /  t;
 
-        _columns[2].z = -(far + near) / r;
-        _columns[2].w = -1.0f;
+        columns[2].z = -(far + near) / r;
+        columns[2].w = -1.0f;
 
-        _columns[3].z = -(2.0f * far * near) / r;
-        _columns[3].w =   0.0f;
+        columns[3].z = -(2.0f * far * near) / r;
+        columns[3].w =   0.0f;
     }
 
     auto mat4::translate(const vec3& vec) -> void
     {
-        _columns[3] += _columns[0] * vec.x +
-                       _columns[1] * vec.y +
-                       _columns[2] * vec.z;
+        columns[3] += columns[0] * vec.x +
+                      columns[1] * vec.y +
+                      columns[2] * vec.z;
     }
 
     auto mat4::scale(const vec3& vec) -> void
     {
-        _columns[0] *= vec.x;
-        _columns[1] *= vec.y;
-        _columns[2] *= vec.z;
+        columns[0] *= vec.x;
+        columns[1] *= vec.y;
+        columns[2] *= vec.z;
     }
 }
--- math/mat4.hpp
+++ math/mat4.hpp
@@ -6,7 +6,7 @@ namespace math
 {
     struct mat4
     {
-        mat4();
+        constexpr mat4();
 
         auto ortho(float left, float right, float bottom, float top, float near, float far) -> void;
         auto ortho(float left, float right, float bottom, float top)                        -> void;
@@ -16,7 +16,6 @@ namespace math
         auto   translate(const vec3& vec) -> void;
         auto       scale(const vec3& vec) -> void;
 
-    private:
-        std::array<column, 4> _columns;
+        std::array<column, 4>  columns;
     };
 }
