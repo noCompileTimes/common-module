e24f384c2294d56005315bfe0df89180c2d177a4
iamdramaticleo <iamdramaticleo@gmail.com>
2025-12-06 20:00:31 +0200

    Add `sqrt2` constant to `constants.hpp` and adjust spacing for `pi`.

--- math/constants.hpp
+++ math/constants.hpp
@@ -3,5 +3,7 @@
 namespace math
 {
     constexpr auto epsilon { 1e-6f };
-    constexpr auto  pi     { std::numbers::pi_v<float> };
+
+    constexpr auto sqrt2   { std::numbers::sqrt2_v<float> };
+    constexpr auto pi      { std::numbers::   pi_v<float> };
 }
