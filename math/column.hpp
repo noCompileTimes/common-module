9df6df143d992b47a324f6e90e3e313458336876
iamdramaticleo <iamdramaticleo@gmail.com>
2025-12-10 12:10:32 +0200

    Mark `operator+` and `operator*` in `column` as `[[nodiscard]]` and adjust `noexcept` spacing.

--- math/column.hpp
+++ math/column.hpp
@@ -4,11 +4,11 @@ namespace math
 {
     struct column
     {
-        auto operator+ (const column& other) const noexcept -> column;
-        auto operator* (float         value) const noexcept -> column;
+        auto operator+=(const column& other) noexcept -> column&;
+        auto operator*=(float         value) noexcept -> column&;
 
-        auto operator+=(const column& other)       noexcept -> column&;
-        auto operator*=(float         value)       noexcept -> column&;
+        [[nodiscard]] auto operator+(const column& other) const noexcept -> column;
+        [[nodiscard]] auto operator*(float         value) const noexcept -> column;
 
         float x { };
         float y { };
