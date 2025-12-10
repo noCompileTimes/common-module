696c104164634e28599bf47fb08a27073c42f62d
iamdramaticleo <iamdramaticleo@gmail.com>
2025-12-10 14:20:28 +0200

    Add `vec3` class implementation and update CMake target sources
    Implemented the `vec3` class with key methods like `normalize`, `length`, `dot`, and `cross`. Added operator overloads for arithmetic operations. Updated `CMakeLists.txt` to include `vec3.cpp` in the build target.

--- math/vec3.hpp
+++ math/vec3.hpp
@@ -4,6 +4,20 @@ namespace math
 {
     struct vec3
     {
+        auto      normalize() noexcept -> void;
+
+        auto         length() const noexcept -> float;
+        auto squared_length() const noexcept -> float;
+
+        static auto cross(const vec3& a, const vec3& b) noexcept -> vec3;
+        static auto   dot(const vec3& a, const vec3& b) noexcept -> float;
+
+        auto operator+=(const vec3& other) noexcept -> vec3&;
+        auto operator*=(float       value) noexcept -> vec3&;
+
+        [[nodiscard]] auto operator+(const vec3& other) const noexcept -> vec3;
+        [[nodiscard]] auto operator*(float       value) const noexcept -> vec3;
+
         float x { };
         float y { };
         float z { };
