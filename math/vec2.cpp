f5c1f263a8cfe164cc8501d1e9e14ae83f571c70
iamdramaticleo <iamdramaticleo@gmail.com>
2025-12-18 15:03:11 +0200

    Add `operator-` and `operator-=` to `vec2` class for subtraction operations

--- math/vec2.cpp
+++ math/vec2.cpp
@@ -10,12 +10,29 @@ namespace math
         return *this;
     }
 
+    auto vec2::operator-=(const vec2& other) noexcept -> vec2&
+    {
+        x -= other.x;
+        y -= other.y;
+
+        return *this;
+    }
+
     auto vec2::operator+(const vec2& other) const noexcept -> vec2
     {
         return
         {
             x + other.x,
             y + other.y
+        };
+    }
+
+    auto vec2::operator-(const vec2& other) const noexcept -> vec2
+    {
+        return
+        {
+            x - other.x,
+            y - other.y
         };
     }
 }
--- math/vec2.hpp
+++ math/vec2.hpp
@@ -5,10 +5,10 @@ namespace math
     struct vec2
     {
         auto operator+=(const vec2& other) noexcept -> vec2&;
+        auto operator-=(const vec2& other) noexcept -> vec2&;
 
         [[nodiscard]] auto operator+(const vec2& other) const noexcept -> vec2;
-
-        // TODO minus operator
+        [[nodiscard]] auto operator-(const vec2& other) const noexcept -> vec2;
 
         float x;
         float y;
