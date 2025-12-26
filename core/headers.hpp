b111bb33c3a4b9857046af525368426b1ab9d86f
iamdramaticleo <iamdramaticleo@gmail.com>
2025-12-26 03:50:23 +0200

    Reorder includes in `headers.hpp` for logical grouping and consistency

--- core/headers.hpp
+++ core/headers.hpp
@@ -10,7 +10,7 @@
 #include "data/camera.hpp"
 #include "data/transform.hpp"
 
-#include "vertex/attribute.hpp"
-
 #include "vertex/type/basic.hpp"
 #include "vertex/type/sprite.hpp"
+
+#include "vertex/attribute.hpp"
