7e4667d51eb0e18cf723ff9b4cf2e2e33ae4c3f7
iamdramaticleo <iamdramaticleo@gmail.com>
2026-01-08 21:39:27 +0200

    Inline `vec2` definitions into header and remove `vec2.cpp`
    Moved all `vec2` method definitions into `vec2.hpp` and deleted `vec2.cpp` to simplify file structure. Updated `CMakeLists.txt` to reflect the removed file.

--- math/vec2.hpp
+++ math/vec2.hpp
@@ -4,11 +4,39 @@ namespace math
 {
     struct vec2
     {
-        auto operator+=(const vec2& other) noexcept -> vec2&;
-        auto operator-=(const vec2& other) noexcept -> vec2&;
+        auto operator+=(const vec2& other) noexcept -> vec2&
+        {
+            x += other.x;
+            y += other.y;
 
-        [[nodiscard]] auto operator+(const vec2& other) const noexcept -> vec2;
-        [[nodiscard]] auto operator-(const vec2& other) const noexcept -> vec2;
+            return *this;
+        }
+
+        auto operator-=(const vec2& other) noexcept -> vec2&
+        {
+            x -= other.x;
+            y -= other.y;
+
+            return *this;
+        }
+
+        [[nodiscard]] auto operator+(const vec2& other) const noexcept -> vec2
+        {
+            return
+            {
+                x + other.x,
+                y + other.y
+            };
+        }
+
+        [[nodiscard]] auto operator-(const vec2& other) const noexcept -> vec2
+        {
+            return
+            {
+                x - other.x,
+                y - other.y
+            };
+        }
 
         union
         {
