caa1b8a8d7d37703e0ced5ad3f21a31217d42a85
iamdramaticleo <iamdramaticleo@gmail.com>
2026-01-08 22:29:10 +0200

    Inline `vec3` definitions into header and remove `vec3.cpp`
    Moved all `vec3` method definitions into `vec3.hpp` and deleted `vec3.cpp` to simplify file structure. Updated `CMakeLists.txt` to reflect the removed file.

--- math/vec3.hpp
+++ math/vec3.hpp
@@ -4,21 +4,102 @@ namespace math
 {
     struct vec3
     {
-        auto normalize() noexcept -> void;
+        auto vec3::normalize() noexcept -> void
+        {
+            if (const auto len = length();
+                           len > epsilon)
+            {
+                const auto i = 1.0f / len;
 
-        [[nodiscard]] auto         length() const noexcept -> float;
-        [[nodiscard]] auto squared_length() const noexcept -> float;
+                x *= i;
+                y *= i;
+                z *= i;
+            }
+        }
 
-        static auto cross(const vec3& a, const vec3& b) noexcept -> vec3;
-        static auto   dot(const vec3& a, const vec3& b) noexcept -> float;
+        [[nodiscard]] auto vec3::length() const noexcept -> float
+        {
+            return sqrt(squared_length());
+        }
 
-        auto operator+=(const vec3& other) noexcept ->  vec3&;
-        auto operator-=(const vec3& other) noexcept ->  vec3&;
-        auto operator*=(float       value) noexcept ->  vec3&;
+        [[nodiscard]] auto vec3::squared_length() const noexcept -> float
+        {
+            return x * x + y * y + z * z;
+        }
 
-        [[nodiscard]] auto operator+(const vec3& other) const noexcept -> vec3;
-        [[nodiscard]] auto operator-(const vec3& other) const noexcept -> vec3;
-        [[nodiscard]] auto operator*(float       value) const noexcept -> vec3;
+        [[nodiscard]] static auto vec3::cross(const vec3& a, const vec3& b) noexcept -> vec3
+        {
+            return
+            {
+                a.y * b.z - a.z * b.y,
+                a.z * b.x - a.x * b.z,
+                a.x * b.y - a.y * b.x
+            };
+        }
+
+        static auto vec3::dot(const vec3& a, const vec3& b) noexcept -> float
+        {
+            return a.x * b.x +
+                   a.y * b.y +
+                   a.z * b.z;
+        }
+
+        auto vec3::operator+=(const vec3& other) noexcept -> vec3&
+        {
+            x += other.x;
+            y += other.y;
+            z += other.z;
+
+            return *this;
+        }
+
+        auto vec3::operator-=(const vec3& other) noexcept -> vec3&
+        {
+            x -= other.x;
+            y -= other.y;
+            z -= other.z;
+
+            return *this;
+        }
+
+        auto vec3::operator*=(const float value) noexcept -> vec3&
+        {
+            x *= value;
+            y *= value;
+            z *= value;
+
+            return *this;
+        }
+
+        [[nodiscard]] auto vec3::operator+(const vec3& other) const noexcept -> vec3
+        {
+            return
+            {
+                x + other.x,
+                y + other.y,
+                z + other.z
+            };
+        }
+
+        [[nodiscard]] auto vec3::operator-(const vec3& other) const noexcept -> vec3
+        {
+            return
+            {
+                x - other.x,
+                y - other.y,
+                z - other.z
+            };
+        }
+
+        [[nodiscard]] auto vec3::operator*(const float value) const noexcept -> vec3
+        {
+            return
+            {
+                x * value,
+                y * value,
+                z * value
+            };
+        }
 
         float x;
         float y;
