53a4805781d5465cafea1f6d8d5416e17ffaf141
iamdramaticleo <iamdramaticleo@gmail.com>
2026-02-06 16:28:29 +0200

    Refactor namespaces and header structure for consistency
    Updated `core::data` to `core::binding` for buffer and texture enums. Introduced a new `internal.hpp` in the math module to consolidate includes, and adjusted related files accordingly. These changes improve code organization and maintainability.

--- math/headers.hpp
+++ math/headers.hpp
@@ -7,12 +7,3 @@
 
 #include <cmath>
 
-#include "constants.hpp"
-#include "functions.hpp"
-
-#include "rgb.hpp"
-
-#include "vec2.hpp"
-#include "vec3.hpp"
-
-#include "quat.hpp"
---
+++ math/internal.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "constants.hpp"
+#include "functions.hpp"
+
+#include "rgb.hpp"
+
+#include "vec2.hpp"
+#include "vec3.hpp"
+
+#include "quat.hpp"
