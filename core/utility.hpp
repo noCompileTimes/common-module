cd6d63be54f312cf1f823b257f1bfcb9811a7175
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-10 14:05:46 +0200

    Add `as_base` utility and refactor bindings to use it
    Introduced the `as_base` utility to simplify conversions of enum values to their underlying types. Refactored various buffer and texture binding calls to use `core::as_base` for improved clarity and consistency. Removed redundant include in `headers.hpp`.

--- core/utility.hpp
+++ core/utility.hpp
@@ -2,17 +2,24 @@
 
 namespace core
 {
-    template <typename type>
-              requires std::is_trivially_copyable_v<type>
-    static auto as_bytes(const std::vector<type>& data) noexcept -> std::span<const std::byte>
+      template <typename type>
+                requires std::is_trivially_copyable_v<type>
+    static auto as_bytes(const std::vector<type>& data) noexcept
     {
         return std::as_bytes(std::span { data });
     }
 
-    template <typename type>
-              requires std::is_class_v<type>
-    static auto as_bytes(const type& object) noexcept -> std::span<const std::byte>
+      template <typename type>
+                requires std::is_class_v<type>
+    static auto as_bytes(const type& object) noexcept
     {
         return std::as_bytes(std::span<const type> { &object, 1 });
+    }
+
+      template <typename type>
+                requires std::is_enum_v<type>
+    static auto as_base(const type value) noexcept
+    {
+        return std::to_underlying(value);
     }
 }
