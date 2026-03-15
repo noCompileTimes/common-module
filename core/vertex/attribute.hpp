52d339b834c0e888429d5c1f12205cb20644836b
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-15 20:37:00 +0200

    Remove default member initializers in `attribute` struct
    Simplifies the `attribute` struct by removing default member initializers, aligning with project conventions.

--- core/vertex/attribute.hpp
+++ core/vertex/attribute.hpp
@@ -4,9 +4,9 @@ namespace core::vertex
 {
     struct attribute
     {
-        uint32_t  index { };
-         int32_t   size { };
-        uint32_t   type { };
-        uint32_t offset { };
+        uint32_t  index;
+         int32_t   size;
+        uint32_t   type;
+        uint32_t offset;
     };
 }
