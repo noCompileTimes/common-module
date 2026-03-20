fa341be9a7a618afde03ed77e5ae1ada9a30a7bb
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-20 12:44:53 +0200

    Reorder includes in `internal.hpp` for consistency

--- core/internal.hpp
+++ core/internal.hpp
@@ -3,8 +3,8 @@
 #include "data/camera.hpp"
 #include "data/transform.hpp"
 
-#include "vertex/attribute.hpp"
-
 #include "vertex/type/basic.hpp"
 #include "vertex/type/model.hpp"
 #include "vertex/type/sprite.hpp"
+
+#include "vertex/attribute.hpp"
