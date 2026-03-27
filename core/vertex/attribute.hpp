a722e24fbbd06a563b30c2295180cdb5ca5b17ae
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-27 11:47:38 +0200

    Change `size` type in `attribute` struct from `int32_t` to `uint32_t` for consistency

--- core/vertex/attribute.hpp
+++ core/vertex/attribute.hpp
@@ -5,7 +5,7 @@ namespace core::vertex
     struct attribute
     {
         uint32_t  index;
-         int32_t   size;
+        uint32_t   size;
         uint32_t   type;
         uint32_t offset;
     };
