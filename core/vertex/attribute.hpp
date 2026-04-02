88637c4896344796d614263fbb23ee59f004b4a9
iamdramaticleo <iamdramaticleo@gmail.com>
2026-04-02 12:12:17 +0300

    Standardize integer types in OpenGL buffer functions to `std::` prefixed variants

--- core/vertex/attribute.hpp
+++ core/vertex/attribute.hpp
@@ -4,9 +4,9 @@ namespace core::vertex
 {
     struct attribute
     {
-        uint32_t  index;
-        uint32_t   size;
-        uint32_t   type;
-        uint32_t offset;
+        std::uint32_t  index;
+        std::uint32_t   size;
+        std::uint32_t   type;
+        std::uint32_t offset;
     };
 }
