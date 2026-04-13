b387f649ab6f26ae8733410d7b16e56453183d03
iamdramaticleo <iamdramaticleo@gmail.com>
2026-04-13 23:07:20 +0300

    Simplify axis vertex color definitions using constexpr variables.

--- core/axis.hpp
+++ core/axis.hpp
@@ -9,19 +9,19 @@ namespace core
     public:
         static auto create(const math::vec3& scale) noexcept -> geometry<vertex::type::editor>
         {
-            const auto hx = scale.x * 0.5f;
-            const auto hy = scale.y * 0.5f;
-            const auto hz = scale.z * 0.5f;
+            const auto hx = scale.x * 0.5f;  constexpr math::vec3 r = { 1.0f, 0.0f, 0.0f };
+            const auto hy = scale.y * 0.5f;  constexpr math::vec3 g = { 0.0f, 1.0f, 0.0f };
+            const auto hz = scale.z * 0.5f;  constexpr math::vec3 b = { 0.0f, 0.0f, 1.0f };
 
             return
             {
                 {
-                    { { -hx,    0.0f,  0.0f }, { 1.0f, 0.0f, 0.0f } }, // 0
-                    { {  hx,    0.0f,  0.0f }, { 1.0f, 0.0f, 0.0f } }, // 1
-                    { {  0.0f,  hy,    0.0f }, { 0.0f, 1.0f, 0.0f } }, // 2
-                    { {  0.0f, -hy,    0.0f }, { 0.0f, 1.0f, 0.0f } }, // 3
-                    { {  0.0f,  0.0f,  hz   }, { 0.0f, 0.0f, 1.0f } }, // 4
-                    { {  0.0f,  0.0f, -hz   }, { 0.0f, 0.0f, 1.0f } }  // 5
+                    { { -hx,    0.0f,  0.0f }, r }, // 0
+                    { {  hx,    0.0f,  0.0f }, r }, // 1
+                    { {  0.0f,  hy,    0.0f }, g }, // 2
+                    { {  0.0f, -hy,    0.0f }, g }, // 3
+                    { {  0.0f,  0.0f,  hz   }, b }, // 4
+                    { {  0.0f,  0.0f, -hz   }, b }  // 5
                 },
                 {
                     0, 1, // x
