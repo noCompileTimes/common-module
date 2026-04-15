931f8bb6e8ac30ac59e5d1456011c7e8d0c24e59
iamdramaticleo <iamdramaticleo@gmail.com>
2026-04-16 00:14:31 +0300

    Add TODO comments for binary mode default and `char` support in `file::read` function

--- core/file.hpp
+++ core/file.hpp
@@ -5,10 +5,10 @@ namespace core
     class File
     {
     public:
-        template <typename type = std::byte>
+        template <typename type = std::byte>                              // TODO which should be the value for mode if you don't want binary?
         [[nodiscard]] static auto read(const std::filesystem::path& path, const std::ios::openmode mode = std::ios::binary) -> std::vector<type>
         {
-            static_assert(sizeof(type) == 1, "file read requires byte-sized types");
+            static_assert(sizeof(type) == 1, "file read requires byte-sized types"); // TODO check if this is supported with char
 
             assert(is_regular_file(path));
 
