fdd1eeab296afbf05879e117735417ea75813297
iamdramaticleo <iamdramaticleo@gmail.com>
2026-04-30 15:51:50 +0300

    Simplify attribute offset usage in VAO attachments
    Replaced explicit `.x` component offsets with direct usage of struct attributes for better clarity and maintainability. Updated related comments and removed unnecessary offset references in debug code.

--- core/vertex/attribute.hpp
+++ core/vertex/attribute.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 namespace core::vertex
-{
-    struct attribute // TODO move this back to graphics? it makes sense to be there???
+{                    // TODO add the binding somewhere around here
+    struct attribute // TODO move this back to graphics in opengl folder? it makes sense to be there?
     {
         uint32_t index;
         uint32_t start;
