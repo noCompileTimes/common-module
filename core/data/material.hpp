f18cdc2f32ea1dc03c81dec80489c044446cf656
iamdramaticleo <iamdramaticleo@gmail.com>
2026-04-30 11:08:05 +0300

    Refactor uniform buffer storage and include new material type
    Updated uniform buffer storage to use appropriate data structures for camera, transform, and material. Added a new `material` type with initial properties and adjusted includes for better modularization.

---
+++ core/data/material.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace core::data
+{
+    struct material
+    {
+        math::rgb albedo_color; // TODO just color as name at some point? - applied also in the shaders
+    };
+}
--- core/internal.hpp
+++ core/internal.hpp
@@ -1,8 +1,5 @@
 #pragma once
 
-#include "data/camera.hpp"
-#include "data/transform.hpp"
-
 #include "vertex/type/editor.hpp"
 #include "vertex/type/model.hpp"
 #include "vertex/type/sprite.hpp"
