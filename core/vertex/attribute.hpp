498c6d12cea6929a2cca79aba613988c77277014
iamdramaticleo <iamdramaticleo@gmail.com>
2026-04-30 10:29:07 +0300

    Refactor vertex attribute structure and VAO attachment.
    Reorganized `attribute` structure by renaming `offset` to `start` and adjusting its order. Updated all VAO attachment calls to reflect the new attribute structure for improved clarity and consistency. Adjusted `glVertexArrayAttribFormat` usage to accommodate these changes.

--- core/vertex/attribute.hpp
+++ core/vertex/attribute.hpp
@@ -4,9 +4,9 @@ namespace core::vertex
 {
     struct attribute
     {
-        std::uint32_t  index;
-        std::uint32_t   size;
-        std::uint32_t   type;
-        std::uint32_t offset;
+        std::uint32_t index;
+        std::uint32_t start;
+        std::uint32_t  size;
+        std::uint32_t  type;
     };
 }
