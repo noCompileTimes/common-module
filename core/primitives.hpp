4f02eefb01d4e64d0f145be70e8f8da7f68d9474
iamdramaticleo <iamdramaticleo@gmail.com>
2026-05-15 14:01:44 +0300

    Use constexpr value in `create_bounding_box` to improve code clarity

--- core/primitives.hpp
+++ core/primitives.hpp
@@ -68,9 +68,11 @@ namespace core
 
         static auto create_bounding_box(const math::vec3& scale, const math::vec3& color) noexcept -> vertex::geometry<> // TODO return return type with auto
         {                                                                                                                // TODO use here a static geometry because we know all the vertices = no allocation
-            const auto hx = scale.x * 0.5f;
-            const auto hy = scale.y * 0.5f;
-            const auto hz = scale.z * 0.5f;
+            constexpr auto value = 0.5f;
+
+            const auto hx = scale.x * value;
+            const auto hy = scale.y * value;
+            const auto hz = scale.z * value;
 
             return
             {
