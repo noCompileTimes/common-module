3f75315224981680855a5e366fd17c16689de443
iamdramaticleo <iamdramaticleo@gmail.com>
2026-05-21 00:18:44 +0300

    Reorder constants in `constants.hpp` for better readability and consistency

--- math/constants.hpp
+++ math/constants.hpp
@@ -2,8 +2,8 @@
 
 namespace math
 {
-    constexpr auto pi      = 3.141592653589793f;
-    constexpr auto sqrt2   = 1.4142135623730951f;
-
     constexpr auto epsilon = 1e-6f;
+
+    constexpr auto sqrt2 = 1.4142135623730951f;
+    constexpr auto pi    = 3.141592653589793f;
 }
