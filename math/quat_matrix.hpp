c9f931e011120d30799b97500e82ba35733012e4
iamdramaticleo <iamdramaticleo@gmail.com>
2026-05-23 23:38:05 +0300

    Add TODO for potential mat4 optimization in quaternion-to-matrix conversion

--- math/quat_matrix.hpp
+++ math/quat_matrix.hpp
@@ -19,7 +19,7 @@ namespace math
 
         const auto zz = quat.z * quat.z;
 
-        matrix[0].x = 1.0f - 2.0f * (yy + zz);
+        matrix[0].x = 1.0f - 2.0f * (yy + zz); // TODO it seems that many values are the same as the identity matrix, maybe use the mat4 { 1.0f }?
         matrix[0].y =        2.0f * (xy + wz);
         matrix[0].z =        2.0f * (xz - wy);
         matrix[0].w =        0.0f;
