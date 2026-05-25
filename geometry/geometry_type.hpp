5fe8e3eb3d7d536046b3769d9b81c70fb055c0a3
iamdramaticleo <iamdramaticleo@gmail.com>
2026-05-25 12:02:04 +0300

    Add `gizmo` geometry type using `vertex::basic` and `primitive::line`

--- geometry/geometry_type.hpp
+++ geometry/geometry_type.hpp
@@ -12,5 +12,6 @@ namespace geometry // TODO remove this file?
 {
     using  model = geometry<vertex::model>;
     using sprite = geometry<vertex::sprite, primitive::triangle>;
+
     using  gizmo = geometry<vertex::basic,  primitive::line>;
 }
