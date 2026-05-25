e828484f9db19400b87aa693bdade5b64456b7ef
iamdramaticleo <iamdramaticleo@gmail.com>
2026-05-25 12:02:28 +0300

    Fix alignment in `geometry_type.hpp` for consistent formatting

--- geometry/geometry_type.hpp
+++ geometry/geometry_type.hpp
@@ -13,5 +13,5 @@ namespace geometry // TODO remove this file?
     using  model = geometry<vertex::model>;
     using sprite = geometry<vertex::sprite, primitive::triangle>;
 
-    using  gizmo = geometry<vertex::basic,  primitive::line>;
+    using  gizmo = geometry<vertex::basic, primitive::line>;
 }
