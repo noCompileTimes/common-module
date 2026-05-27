12e9dc670e4eaac53efc57c58cb20e6ea027560f
iamdramaticleo <iamdramaticleo@gmail.com>
2026-05-27 14:11:05 +0300

    Refactor variable naming in `gizmo.hpp` and `primitive.hpp` for consistency; replace redundant `value` with `half`.

--- geometry/gizmo.hpp
+++ geometry/gizmo.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "math/vec3.hpp" // TODO remove redundant include
-
 #include "geometry_type.hpp"
 
 namespace geometry
@@ -11,21 +9,23 @@ namespace geometry
     public:
         static auto axis(const math::vec3& scale) noexcept // TODO use here a static geometry because we know all the vertices = no allocation
         {
-            constexpr auto value = 0.5f;
+            constexpr auto half = 0.5f;
 
-            const auto hx = scale.x * value; constexpr math::vec3 r { 1.0f, 0.0f, 0.0f };
-            const auto hy = scale.y * value; constexpr math::vec3 g { 0.0f, 1.0f, 0.0f };
-            const auto hz = scale.z * value; constexpr math::vec3 b { 0.0f, 0.0f, 1.0f };
+            const auto x = scale.x * half; constexpr math::vec3 r { 1.0f, 0.0f, 0.0f };
+            const auto y = scale.y * half; constexpr math::vec3 g { 0.0f, 1.0f, 0.0f };
+            const auto z = scale.z * half; constexpr math::vec3 b { 0.0f, 0.0f, 1.0f };
 
             return gizmo
             {
                 {
-                    { {  -hx,  0.0f,  0.0f }, r },
-                    { {   hx,  0.0f,  0.0f }, r },
-                    { { 0.0f,   -hy,  0.0f }, g },
-                    { { 0.0f,    hy,  0.0f }, g },
-                    { { 0.0f,  0.0f,   -hz }, b },
-                    { { 0.0f,  0.0f,    hz }, b }
+                    { { -x, 0.0f, 0.0f }, r },
+                    { {  x, 0.0f, 0.0f }, r },
+
+                    { { 0.0f, -y, 0.0f }, g },
+                    { { 0.0f,  y, 0.0f }, g },
+
+                    { { 0.0f, 0.0f, -z }, b },
+                    { { 0.0f, 0.0f,  z }, b }
                 },
                 {
                     { 0, 1 },
--- geometry/primitive.hpp
+++ geometry/primitive.hpp
@@ -67,23 +67,23 @@ namespace geometry
 
         static auto bounding_box(const math::vec3& scale, const math::vec3& color) noexcept // TODO use here a static geometry because we know all the vertices = no allocation
         {
-            constexpr auto value = 0.5f;
+            constexpr auto half = 0.5f;
 
-            const auto hx = scale.x * value; // TODO for this use an aabb instead of the scale? it makes more sens if you want to have more control over the bounding box - like in sprites with rect
-            const auto hy = scale.y * value;
-            const auto hz = scale.z * value;
+            const auto x = scale.x * half; // TODO for this use an aabb instead of the scale? it makes more sens if you want to have more control over the bounding box - like in sprites with rect
+            const auto y = scale.y * half;
+            const auto z = scale.z * half;
 
             return geometry // TODO use lines for this
             {
                 {
-                    { { -hx, -hy, -hz }, color },
-                    { {  hx, -hy, -hz }, color },
-                    { {  hx,  hy, -hz }, color },
-                    { { -hx,  hy, -hz }, color },
-                    { { -hx, -hy,  hz }, color },
-                    { {  hx, -hy,  hz }, color },
-                    { {  hx,  hy,  hz }, color },
-                    { { -hx,  hy,  hz }, color }
+                    { { -x, -y, -z }, color },
+                    { {  x, -y, -z }, color },
+                    { {  x,  y, -z }, color },
+                    { { -x,  y, -z }, color },
+                    { { -x, -y,  z }, color },
+                    { {  x, -y,  z }, color },
+                    { {  x,  y,  z }, color },
+                    { { -x,  y,  z }, color }
                 },
                 {
                     0, 1,  1, 2,  2, 3,  3, 0,
