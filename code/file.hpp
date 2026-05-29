c0c03f2d0b2f623bc95f6d606a003fa46af2c570
iamdramaticleo <iamdramaticleo@gmail.com>
2026-05-29 22:37:31 +0300

    Refactor includes and update math constants to use `<numbers>` header. Adjust CMakeLists for accurate include paths.

--- code/file.hpp
+++ code/file.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <fstream>
 #include <filesystem>
+#include <fstream>
 
 class File
 {
--- code/math/constants.hpp
+++ code/math/constants.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <numbers>
+
 namespace math
 {
     constexpr auto epsilon = 1e-6f;
 
-    constexpr auto sqrt2   = 1.4142135623730951f; // TODO I should use here the numbers header?
-    constexpr auto pi      = 3.141592653589793f;  // TODO I should use here the numbers header?
+    constexpr auto sqrt2   = std::numbers::sqrt2_v<float>;
+    constexpr auto pi      = std::numbers::   pi_v<float>;
 }
